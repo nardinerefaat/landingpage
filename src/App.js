00b4654a0dd8c68ceeed14761037056a3ab8f5c5
Nardine Refaat <nardinerefaat2@gmail.com>
2026-03-02 19:06:51 +0200

    updated landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import Hero from './components/hero';
+import Features from './components/features'
+import Calltoaction from './components/calltoaction';
+import Pricing from './components/pricing';
+import Testimonials from './components/testimonials';
 
 function App() {
   return (
     <div className="App">
-     
+      <Hero/>
+      <Features/>
+      <Calltoaction/>
+      <Pricing/>
+      <Testimonials/>
     </div>
   );
 }
